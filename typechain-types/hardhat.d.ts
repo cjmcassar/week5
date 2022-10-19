6f6fa2f9fb168560276cc7d72f1e066dc9dcb7f3
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-10-19 17:34:19 -0400

    *

--- typechain-types/hardhat.d.ts
+++ typechain-types/hardhat.d.ts
@@ -13,24 +13,69 @@ import * as Contracts from ".";
 declare module "hardhat/types/runtime" {
   interface HardhatEthersHelpers extends HardhatEthersHelpersBase {
     getContractFactory(
+      name: "CorrectSortedBallot",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.CorrectSortedBallot__factory>;
+    getContractFactory(
       name: "Gas",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Gas__factory>;
     getContractFactory(
+      name: "NotQuiteRandom",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.NotQuiteRandom__factory>;
+    getContractFactory(
       name: "OverflowTest",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.OverflowTest__factory>;
+    getContractFactory(
+      name: "PseudoRandom",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.PseudoRandom__factory>;
+    getContractFactory(
+      name: "Random",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.Random__factory>;
+    getContractFactory(
+      name: "SortedBallot",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.SortedBallot__factory>;
 
+    getContractAt(
+      name: "CorrectSortedBallot",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.CorrectSortedBallot>;
     getContractAt(
       name: "Gas",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.Gas>;
     getContractAt(
+      name: "NotQuiteRandom",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.NotQuiteRandom>;
+    getContractAt(
       name: "OverflowTest",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.OverflowTest>;
+    getContractAt(
+      name: "PseudoRandom",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.PseudoRandom>;
+    getContractAt(
+      name: "Random",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Random>;
+    getContractAt(
+      name: "SortedBallot",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.SortedBallot>;
 
     // default types
     getContractFactory(
